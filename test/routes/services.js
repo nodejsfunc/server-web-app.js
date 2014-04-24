60fcccc3049f13ed1e7dce8910054822316deab9
Vilmos Ioo <vilmosi@blinkbox.com>
2014-04-24 11:03:03 +0100

    Added more service tests

--- test/routes/services.js
+++ test/routes/services.js
@@ -6,10 +6,12 @@ var express = require('express'),
 	request = require('supertest')('http://localhost:3000'),
 	bodyParser = require('body-parser'),
 	cookieParser = require('cookie-parser'),
+	querystring = require('querystring'),
 	nock = require('nock'),
 	mock = {
 		path: 'path',
-		response: 'some response'
+		response: 'some response body',
+		request: {'some request body': 1}
 	},
 	methods = ['get', 'patch', 'delete', 'post'];
 
@@ -29,7 +31,6 @@ var express = require('express'),
 			server.close();
 		});
 
-		// todo add more rigurous tests, like comparing headers between the req and proxy
 		for(var i = 0, l = domains.length; i < l; i++){
 			(function(domain){
 				var	proxy = config.api_domains[domain].options.port === 443 ? 'https' : 'http';
@@ -45,6 +46,61 @@ var express = require('express'),
 									.reply(200, mock.response);
 
 								request[method]('/' + domain + '/' + mock.path)
+									.expect(200, mock.response, done);
+							});
+						})(methods[i]);
+					}
+				});
+			})(domains[i]);
+		}
+
+		for(var i = 0, l = domains.length; i < l; i++){
+			(function(domain){
+				var	proxy = config.api_domains[domain].options.port === 443 ? 'https' : 'http';
+				proxy += '://' + config.api_domains[domain].options.host;
+
+				describe('should copy request headers and return proxy response headers for /' + domain + '/path', function(){
+					for(var i = 0, l = methods.length; i < l; i++){
+						(function(method){
+							it(method.toUpperCase(), function(done){
+								// mock proxy request
+								nock(proxy)
+									.intercept((config.api_domains[domain].root && '/' + config.api_domains[domain].root) + '/' + mock.path, method)
+									.matchHeader('x-server-request', 'blinkbox')
+									.reply(200, mock.response, {
+										'x-server-response': 'blinkbox'
+									});
+
+								request[method]('/' + domain + '/' + mock.path)
+									.set('x-server-request', 'blinkbox')
+									.expect('x-server-response', 'blinkbox')
+									.expect(200, mock.response, done);
+							});
+						})(methods[i]);
+					}
+				});
+			})(domains[i]);
+		}
+
+		methods = ['post', 'patch']; // only testing these methods now
+
+		for(var i = 0, l = domains.length; i < l; i++){
+			(function(domain){
+				var	proxy = config.api_domains[domain].options.port === 443 ? 'https' : 'http';
+				proxy += '://' + config.api_domains[domain].options.host;
+
+				describe('should send the body of the request with the proxy for /' + domain + '/path', function(){
+					for(var i = 0, l = methods.length; i < l; i++){
+						(function(method){
+							it(method.toUpperCase(), function(done){
+								// mock proxy request
+								// the body is transformed by nodejs using the querystring
+								nock(proxy)
+									.intercept((config.api_domains[domain].root && '/' + config.api_domains[domain].root) + '/' + mock.path, method, querystring.stringify(mock.request))
+									.reply(200, mock.response);
+
+								request[method]('/' + domain + '/' + mock.path)
+									.send(mock.request)
 									.expect(200, mock.response, done);
 							});
 						})(methods[i]);
