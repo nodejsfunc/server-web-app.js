1464e0307f4fc2de6822d5ddea98b3744d8beb35
Vilmos Ioo <vilmosi@blinkbox.com>
2014-04-24 15:54:25 +0100

    Improved options test

--- app/scripts/error.js
+++ app/scripts/error.js
@@ -10,7 +10,6 @@ module.exports = function(err, req, res, next){
 		res.statusCode = 504;
 		res.end('timeout of ' + err.timeout + 'ms exceeded');
 	} else {
-		console.log(err);
 		var response = JSON.stringify({ code: 500, error: '500 Oh oh! something broke!' });
 		res.setHeader('X-Powered-By', constants.APP_NAME + constants.APP_VERSION);
 		res.setHeader('Content-Type', 'application/json');
--- test/scripts/options.js
+++ test/scripts/options.js
@@ -5,6 +5,7 @@ var express = require('express'),
 	middleware = require('../../app/scripts/options'),
 	cookieParser = require('cookie-parser'),
 	path = '/:domain/*',
+	extend = require('extend'),
 	request = require('supertest')('http://localhost:3000');
 
 describe('Options ', function(){
@@ -16,7 +17,7 @@ describe('Options ', function(){
 		app.use(cookieParser());
 		app.use(path, middleware);
 		app.get(path, function(req, res){
-			res.send(200);
+			res.send(200, req.options);
 		});
 		server = app.listen(3000);
 	});
@@ -29,9 +30,29 @@ describe('Options ', function(){
 	for(var i = 0, l = domains.length; i < l; i++){
 		(function(domain){
 			it('should create options obj for /' + domain, function(done){
+
+				// build expected options
+				// should derive from the original static options
+				var options = extend(true, {}, config.api_domains[domain].options);
+
+				// should have original request headers (default and custom)
+				options.headers = {
+					'accept-encoding': 'gzip, deflate',
+					connection: 'close',
+					origin: 'http://localhost:3000',
+					'x-custom-header': 1
+				};
+
+				// should include request method
+				options.method = 'GET';
+
+				// should include path
+				options.path = (config.api_domains[domain].root && '/' + config.api_domains[domain].root) + '/path';
+
 				request
 					.get('/'+domain + '/path')
-					.expect(200, done);
+					.set('x-custom-header', 1)
+					.expect(200, options, done);
 			});
 		})(domains[i]);
 	}
