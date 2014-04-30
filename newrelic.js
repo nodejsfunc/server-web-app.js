eb23ac4de6346affc1e01c2c41e36df6c5e3bf8d
Vilmos Ioo <vilmosi@blinkbox.com>
2014-04-30 15:17:55 +0100

    Updated newrelic

--- newrelic.js
+++ newrelic.js
@@ -4,7 +4,7 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-var config = require('./config/config');
+var config = require('./app/config/config');
 
 exports.config = {
   /**
