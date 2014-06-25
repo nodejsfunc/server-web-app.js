6e162f0584501dd9cb7b4192ee2aa62b50698a37
Alex Andrews <alexa@blinkbox.com>
2014-06-25 15:12:09 +0100

    JS lint.

--- test/services/auth.js
+++ test/services/auth.js
@@ -2,7 +2,6 @@
 
 var constants = require('../../app/config/constants'),
     config = require('../../app/config/config'),
-    should = require('should'),
     nock = require('nock');
 
 var auth = require('../../app/services/auth');
@@ -10,8 +9,8 @@ var auth = require('../../app/services/auth');
 describe('Auth service module', function(){
   it('sends a HTTP request to revoke the access token', function(done){
     var authServer = nock('https://' + config.api_domains.auth.options.host)
-        .post(constants.REVOKE_REFRESH_TOKEN)
-        .reply(200);
+      .post(constants.REVOKE_REFRESH_TOKEN)
+      .reply(200);
 
     auth.revokeRefreshToken('some-token-which-is-nonsense').then(function(){
       authServer.done();
@@ -20,9 +19,9 @@ describe('Auth service module', function(){
   });
 
   it('rejects the promise if we do not get a 200', function(done){
-    var authServer = nock('https://' + config.api_domains.auth.options.host)
-        .post(constants.REVOKE_REFRESH_TOKEN)
-        .reply(500);
+    nock('https://' + config.api_domains.auth.options.host)
+      .post(constants.REVOKE_REFRESH_TOKEN)
+      .reply(500);
 
     auth.revokeRefreshToken('some-token-which-is-nonsense').then(null, done);
   });
