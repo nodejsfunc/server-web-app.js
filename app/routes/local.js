d093e952b5312670602886349a216ad565b6c084
estebans <developer@wordcitizen.com>
2014-07-15 10:59:29 +0100

    cover scenario when signin out and access token is not found in redis

--- app/routes/local.js
+++ app/routes/local.js
@@ -10,6 +10,7 @@ var auth = require('./../services/auth');
 
 router
 	.get(constants.SIGN_OUT_PATH, function(req, res){
+
 		var access_token = req.cookies[constants.AUTH_ACCESS_TOKEN_NAME];
 
     if (access_token === undefined) {
@@ -17,20 +18,39 @@ router
     }
 
     repository.get(access_token).then(function(data){
-      data = JSON.parse(data);
 
-      auth.revokeRefreshToken(data.refresh_token).then(function(){
-        repository.del(access_token);
+      try {
+
+        data = JSON.parse(data);
+
+        auth.revokeRefreshToken(data.refresh_token).then(function(){
+
+          repository.del(access_token);
+
+          res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
+
+          res.send(200, null);
+
+        }, function(e){
+
+          res.send(500, e);
+
+        });
+
+      } catch(e) {
 
         res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
 
         res.send(200, null);
-      }, function(e){
-        res.send(500, e);
-      });
+
+      }
+
     }, function(){
+
       res.send(500);
+
     });
+
 	})
 	.get(constants.CLIENT_CONFIG_PATH, function(req, res){
 		res.send(200, config.clientConfig);
