8662cfd794b79c0932ef01a3a6c3ffae5a7a1539
Sebastian Tschan <sebastiant@blinkbox.com>
2014-08-08 11:10:18 +0100

    Removed unnecessary whitespace.

--- app/routes/local.js
+++ app/routes/local.js
@@ -10,49 +10,29 @@ var auth = require('./../services/auth');
 
 router
 	.get(constants.SIGN_OUT_PATH, function(req, res){
-
 		var access_token = req.cookies[constants.AUTH_ACCESS_TOKEN_NAME];
-
     if (access_token === undefined) {
 	    res.set('www-authenticate', constants.NO_TOKEN);
 	    res.send(401);
 	    return;
     }
-
-    repository.get(access_token).then(function(data){
-
+    repository.get(access_token).then(function (data) {
       try {
-
         data = JSON.parse(data);
-
-        auth.revokeRefreshToken(data.refresh_token).then(function(){
-
+        auth.revokeRefreshToken(data.refresh_token).then(function () {
           repository.del(access_token);
-
           res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
-
           res.send(200, null);
-
         }, function(e){
-
           res.send(500, e);
-
         });
-
       } catch(e) {
-
         res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
-
         res.send(200, null);
-
       }
-
     }, function(){
-
       res.send(500);
-
     });
-
 	})
 	.get(constants.CLIENT_CONFIG_PATH, function(req, res){
 		res.send(200, config.clientConfig);
