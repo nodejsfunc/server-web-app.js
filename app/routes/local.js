0381d189b1a640e16170a2f08470d3b965201496
Sebastian Tschan <sebastiant@blinkbox.com>
2014-08-08 11:51:13 +0100

    Log sign out errors.

--- app/routes/local.js
+++ app/routes/local.js
@@ -4,6 +4,7 @@ var express = require('express'),
 	constants = require('./../config/constants'),
 	config = require('./../config/config'),
 	repository = require('./../util/repository'),
+	logger = require('./../util/logger'),
 	router = express.Router();
 
 var auth = require('./../services/auth');
@@ -11,28 +12,28 @@ var auth = require('./../services/auth');
 router
 	.get(constants.SIGN_OUT_PATH, function(req, res){
 		var access_token = req.cookies[constants.AUTH_ACCESS_TOKEN_NAME];
-    if (access_token === undefined) {
+    if (!access_token) {
 	    res.set('www-authenticate', constants.NO_TOKEN);
 	    res.send(401);
 	    return;
     }
+		function errorHandler(err) {
+			res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
+			res.send(500);
+			logger.error(err);
+		}
     repository.get(access_token).then(function (data) {
       try {
         data = JSON.parse(data);
         auth.revokeRefreshToken(data.refresh_token).then(function () {
           repository.del(access_token);
           res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
-          res.send(200, null);
-        }, function(e){
-          res.send(500, e);
-        });
-      } catch(e) {
-        res.clearCookie(constants.AUTH_ACCESS_TOKEN_NAME, { path: '/api' });
-        res.send(200, null);
+          res.send(200);
+        }, errorHandler);
+      } catch (err) {
+	      errorHandler(err);
       }
-    }, function(){
-      res.send(500);
-    });
+    }, errorHandler);
 	})
 	.get(constants.CLIENT_CONFIG_PATH, function(req, res){
 		res.send(200, config.clientConfig);
