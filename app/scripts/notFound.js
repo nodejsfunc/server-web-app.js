28287b557e74bccb20895b3e7f3f8dcf75dc6c6f
Sebastian Tschan <sebastiant@blinkbox.com>
2014-08-08 13:55:29 +0100

    Simplified 404 handler.

--- app/scripts/notFound.js
+++ app/scripts/notFound.js
@@ -3,10 +3,6 @@
 
 var constants = require('../config/constants');
 
-module.exports = function(req, res){
-	var response = JSON.stringify({ code: 404, error: '404 Not found' });
-	res.setHeader('X-Powered-By', constants.APP_NAME + constants.APP_VERSION);
-	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('Content-Length', Buffer.byteLength(response));
-	res.send(404, response);
+module.exports = function (req, res) {
+	res.send(404);
 };
