46e0b434522eb4977a3404371798e890adc55385
Sebastian Tschan <sebastiant@blinkbox.com>
2014-12-01 12:54:28 +0000

    Handle uncaught exceptions via domain error handler.

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var express = require('express'),
 	cookieParser = require('cookie-parser'),
 	timeout = require('connect-timeout'),
 	path = require('path'),
+	domain = require('domain'),
 	port = process.env.PORT || 3000,
 	constants = require('./app/config/constants'),
 	// New Relic and timeout config will not be refreshed on require cache invalidation:
 	config = require('./app/config/config'),
 	middleware = require('./app/scripts'),
 	routes = require('./app/routes'),
-	logger = require('./app/util/logger');
+	logger = require('./app/util/logger'),
+	app,
+	server;
 
 // Allow configuration reload on SIGHUP, see:
 // http://jira.blinkbox.local/jira/browse/SWA-74
@@ -34,9 +37,40 @@ process.on('exit', function (code) {
 });
 
 // Configure application
-var app = express();
+app = express();
 
 app.enable('trust proxy'); // required for nginx
+
+// Handle uncaught exceptions via domain error handler:
+// http://nodejs.org/docs/latest/api/domain.html
+app.use(function(req, res, next) {
+	var requestDomain = domain.create();
+	requestDomain.add(req);
+	requestDomain.add(res);
+	requestDomain.on('error', function (err) {
+		logger.emergency(err);
+		if (server) {
+			try {
+				// make sure we close down within 10 seconds:
+				var killtimer = setTimeout(function() {
+					process.exit(1);
+				}, 10000);
+				// But don't keep the process open just for that:
+				killtimer.unref();
+				// stop taking new requests:
+				server.close();
+				// try to send an error to the request that triggered the problem:
+				res.send(500);
+			} catch (err2) {
+				logger.error(err2);
+			}
+		} else {
+			process.exit(1);
+		}
+	});
+	requestDomain.run(next);
+});
+
 app.use(middleware.requestLogger);
 app.use(middleware.csrfHeader);
 app.use(middleware.poweredByHeader);
@@ -56,5 +90,5 @@ if (config.newRelicKey) {
 }
 
 // Start server
-app.listen(port);
+server = app.listen(port);
 logger.notice('Express server listening on port ' + port);
